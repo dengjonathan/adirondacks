9a50a8a12a2d7a01ce973480222233ab5f2e5611
dengjonathan <dengjonathan@gmail.com>
2016-06-17 18:33:08 -0500

    broken map div

--- app/maps_api.js
+++ app/maps_api.js
@@ -1,46 +1,25 @@
-function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {
-      lat: 44.1899178,
-      lng: -73.7866135
-    },
-    zoom: 12
-  });
-}
+var CENTER_COORD = {
+  lat: 44.1899178,
+  lng: -73.7866135
+};
 
-// TODO: set styles on google map
-map.set('styles', [
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#000000' },
-      { weight: 1.6 }
-    ]
-  }, {
-    featureType: 'road',
-    elementType: 'labels',
-    stylers: [
-      { saturation: -100 },
-      { invert_lightness: true }
-    ]
-  }, {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [
-      { hue: '#ffff00' },
-      { gamma: 1.4 },
-      { saturation: 82 },
-      { lightness: 96 }
-    ]
-  }, {
-    featureType: 'poi.school',
-    elementType: 'geometry',
-    stylers: [
-      { hue: '#fff700' },
-      { lightness: -15 },
-      { saturation: 99 }
-    ]
-  }
-]);
+function initMap() {
+  console.log('Google Map Initiating');
+  
+  var mapDiv = document.getElementById('map');
+
+  var mapOptions = {
+    center: CENTER_COORD,
+    zoom: 12,
+    draggable: true
+  };
+
+  var map = new google.maps.Map(mapDiv, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: CENTER_COORD,
+    title: "Hello World!"
+  });
+
+  marker.setMap(map);
+};
