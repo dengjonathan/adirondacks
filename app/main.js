3998cfc7bd41b3aae53abbcadc3534ac728def80
dengjonathan <dengjonathan@gmail.com>
2016-06-27 21:59:08 -0700

    location/marker functionality restored

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
-
 // And the monster is alive!
 appViewModel = new viewModel(data);
 
 //need to wait until after Yelp API returns to apply Ko bindings
-$.when($.ajax(appViewModel.yelp_settings())).then(ko.applyBindings(appViewModel)).done(appViewModel.init());
+$.when($.ajax(appViewModel.yelp_settings())).then(function() {
+  appViewModel.init();
+  ko.applyBindings(appViewModel);
+});
--- app/view_model.js
+++ app/view_model.js
@@ -174,129 +174,42 @@ viewModel.prototype = {
     });
   },
 
-  // pulls locations from yelp api
-  // getYelp: function() {
-  //   var self = this;
-  //   console.log(this);
-  //   // var locations = self.locations();
-  //   var yelp_API = {
-  //     yelp_url: 'https://api.yelp.com/v2/search?',
-  //     // TODO: find way to access this info without openly displaying
-  // //     consumerSecret: 'fTfDa0IIFU0QzF7caXw3Ba9-bEQ',
-  // //     tokenSecret: 'hH68LJgKUkwLNhUf0Yavw6jdVes',
-  // //     oauth_consumer_key: 'QbAL_pqgAo730xi3DAC2qA',
-  // //     oauth_token: 'aBZbbpsTdhq9869cQVdja221aJaFuDqv',
-  // //     term: 'food',
-  // //     location: 'Keene+Valley+NY',
-  // //     radius: '20000',
-  // //   };
-  //
-  //   // generate nonce key
-  //   function nonce_generate() {
-  //     return (Math.floor(Math.random() * 1e12).toString());
-  //   }
-  //
-  //   var httpMethod = 'GET';
-  //
-  //   var parameters = {
-  //     oauth_consumer_key: yelp_API.oauth_consumer_key,
-  //     oauth_token: yelp_API.oauth_token,
-  //     oauth_nonce: nonce_generate(),
-  //     oauth_timestamp: Math.floor(Date.now() / 1000),
-  //     oauth_signature_method: 'HMAC-SHA1',
-  //     oauth_version: '1.0',
-  //     callback: 'cb',
-  //     term: yelp_API.term,
-  //     location: yelp_API.location,
-  //     radius: yelp_API.radius
-  //   };
-  //
-  //   var encodedSignature = oauthSignature.generate(
-  //     'GET',
-  //     yelp_API.yelp_url,
-  //     parameters,
-  //     yelp_API.consumerSecret,
-  //     yelp_API.tokenSecret, {
-  //       encodeSignature: false
-  //     });
-  //
-  //   parameters.oauth_signature = encodedSignature;
-  //
-  //   var yelp_settings = {
-  //     url: yelp_API.yelp_url,
-  //     data: parameters,
-  //     cache: true, // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-  //     dataType: 'jsonp',
-  //     success: function(results) {
-  //
-  //       console.log('Success yelp AJAX call!');
-  //       results.businesses.forEach(function(e) {
-  //         var arg = {
-  //           name: e.name,
-  //           position: {
-  //             lat: e.location.coordinate.latitude,
-  //             lng: e.location.coordinate.longitude
-  //           },
-  //           desc: e.snippet_text,
-  //           loc_type: 'food',
-  //           phone: e.display_phone,
-  //           image_url: e.image_url,
-  //           mobile_url: e.mobile_url,
-  //           rating: e.rating,
-  //         };
-  //         locations.push(new Location(arg));
-  //       });
-  //       // self.addMarkers();
-  //     },
-  //     fail: function() {
-  //       // Do stuff on fail
-  //       console.log('error during Yelp AJAX call');
-  //     }
-  //   };
-  //   $.when($.ajax(yelp_settings));
-  // },
-
-  /* after this.yelp_results is populated with function this.getYelp
-  this functions constructs new Location object for each result and pushes
-  to this.locations */
-  loadYelp: function() {
-    var self = this;
-    var locations = [];
-    self.yelp_results().forEach(function(e) {
-      var arg = {
-        name: e.name,
-        position: {
-          lat: e.location.coordinate.latitude,
-          lng: e.location.coordinate.longitude
-        },
-        desc: e.snippet_text,
-        loc_type: 'food',
-        phone: e.display_phone,
-        image_url: e.image_url,
-        mobile_url: e.mobile_url,
-        rating: e.rating,
-      };
-      locations.push(new Location(arg));
-    });
-    console.log(locations);
-    return locations;
-  },
-
   // adds location to trip planning array
   addTrip: function(new_loc) {
     console.log('location added to trip')
-    this.trip.push(new_loc)
+    console.log(new_loc)
+    this.trip().push(new_loc)
   },
 
   //removes trip from trip planning array
   removeTrip: function(loc) {
     console.log('location removed from trip');
-    this.trip.remove(loc)
+    this.trip().remove(loc)
+  },
+
+  slideOut: function() {
+    var slideout = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('menu'),
+      'padding': 256,
+      'tolerance': 70
+    });
+    slideout.toggle();
   },
 
   // initializies ViewModel with map and adds markers
-  init: function() {
-  this.loadData();
-  this.initMap();
+    init: function() {
+    this.loadData();
+    this.initMap();
+    this.addMarkers();
   }
 };
+
+// And the monster is alive!
+appViewModel = new viewModel(data);
+
+//need to wait until after Yelp API returns to apply Ko bindings
+$.when($.ajax(appViewModel.yelp_settings())).then(function() {
+  appViewModel.init();
+  ko.applyBindings(appViewModel);
+});
