a2bb7f613a7a044d8d2156fcabd295935ca25567
dengjonathan <dengjonathan@gmail.com>
2016-07-08 14:06:37 -0700

    working site

---
+++ app:secrets.js
@@ -0,0 +1,22 @@
+var SECRETS = {
+    // returns Google Maps API key
+    getGoogleMapsKey: function(){
+      return 'AIzaSyD7QeUbl2kO7TPllayriD04AkBdkoqBTWw';
+    },
+    // returns Yelp consumer secret
+    getConsumerSecret: function() {
+      return 'fTfDa0IIFU0QzF7caXw3Ba9-bEQ';
+    } ,
+    // returns Yelp token Secret
+    getTokenSecret: function(){
+      return 'hH68LJgKUkwLNhUf0Yavw6jdVes';
+    },
+    // returns Yelp oauth_consumer_key
+    getConsumerKey: function(){
+      return 'QbAL_pqgAo730xi3DAC2qA';
+    },
+    // returns Yelp Oauth Token
+    getOAuthToken: function(){
+      return 'aBZbbpsTdhq9869cQVdja221aJaFuDqv';
+    }
+};
