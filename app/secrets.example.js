a0b79084fed1ed483a244c78bfcce3aaf01b07bc
dengjonathan <dengjonathan@gmail.com>
2016-07-11 14:09:41 -0500

    implement reviewers commets

--- app/secrets.example.js
+++ app/secrets.example.js
@@ -4,47 +4,22 @@ API calls work, fill in values below */
 var SECRETS = {
     // returns Google Maps API key
     getGoogleMapsKey: function(){
-      return 'AIzaSyD7QeUbl2kO7TPllayriD04AkBdkoqBTWw';
+      return 'FILL_ME_IN';
     },
     // returns Yelp consumer secret
     getConsumerSecret: function() {
-      return 'fTfDa0IIFU0QzF7caXw3Ba9-bEQ';
+      return 'FILL_ME_IN';
     } ,
     // returns Yelp token Secret
     getTokenSecret: function(){
-      return 'hH68LJgKUkwLNhUf0Yavw6jdVes';
+      return 'FILL_ME_IN';
     },
     // returns Yelp oauth_consumer_key
     getConsumerKey: function(){
-      return 'QbAL_pqgAo730xi3DAC2qA';
+      return 'FILL_ME_IN';
     },
-    // returns Yelp Oauth Token
+    // returns Yelop Oauth Token
     getOAuthToken: function(){
-      return 'aBZbbpsTdhq9869cQVdja221aJaFuDqv';
+      return 'FILL_ME_IN';
     }
 };
-
-//
-//
-// var SECRETS = {
-//     // returns Google Maps API key
-//     getGoogleMapsKey: function(){
-//       return 'FILL_ME_IN';
-//     },
-//     // returns Yelp consumer secret
-//     getConsumerSecret: function() {
-//       return 'FILL_ME_IN';
-//     } ,
-//     // returns Yelp token Secret
-//     getTokenSecret: function(){
-//       return 'FILL_ME_IN';
-//     },
-//     // returns Yelp oauth_consumer_key
-//     getConsumerKey: function(){
-//       return 'FILL_ME_IN';
-//     },
-//     // returns Yelop Oauth Token
-//     getOAuthToken: function(){
-//       return 'FILL_ME_IN';
-//     }
-// };
